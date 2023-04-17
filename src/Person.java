ca785d867786a7fb164f6fa8a09012b2d9302106
Rhothomir <altindasahmet2@gmail.com>
2023-04-17 15:49:29 +0200

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    protected String surname;
+    protected String firstName;
+    protected String street;
+    protected String zipCode;
+    protected String city;
+
+    public void initialize() {
+        // Code to read input and set fields
+    }
+
+    public void print() {
+        System.out.println("Name: Ahmet" + " Altindas ");
+        System.out.println("Address: Draw 21" + ",202 " + " Warsaw" );
+    }
+}
